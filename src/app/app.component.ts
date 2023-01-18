abbb9baf76bfb50f59aedb788eeae0d3faf53533
Rachid <rachidzahar@hotmail.fr>
2023-01-18 16:11:51 +0100

    three faceSnaps ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { FaceSnap } from './face-snap/models/face-snap.model';
 })
 export class AppComponent implements OnInit {
   mySnap!: FaceSnap
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
 
   ngOnInit() {
       this.mySnap = new FaceSnap(
@@ -15,7 +17,21 @@ export class AppComponent implements OnInit {
       'Une super description',
       'https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg',
       new Date(),
-      0 )
+      0 );
+      this.myOtherSnap = new FaceSnap(
+        'Piano',
+        'Un super piano',
+        'https://i0.wp.com/apprendre-a-jouer-du-piano.com/wp-content/uploads/2016/09/piano-queue.jpg',
+        new Date(),
+        0
+      );
+      this.myLastSnap = new FaceSnap(
+        'Circuit training',
+        'Un super sport',
+        'https://medias.toutelanutrition.com/blog/2020/12/banner-circuit-training.jpg',
+        new Date(),
+        0
+      )
   }
 }
 
