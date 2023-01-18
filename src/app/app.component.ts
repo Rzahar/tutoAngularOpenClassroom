3e76db4dfc082b2ab62048215fe2a86747ee0b54
Rachid <rachidzahar@hotmail.fr>
2023-01-18 15:56:38 +0100

    first faceSnap ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './face-snap/models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = '';
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap
+
+  ngOnInit() {
+      this.mySnap = new FaceSnap(
+      'Archibald',
+      'Une super description',
+      'https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg',
+      new Date(),
+      0 )
+  }
 }
+
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap',
@@ -6,28 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./face-snap.component.scss']
 })
 export class FaceSnapComponent implements OnInit {
-  title!: string;
-  description! : string;
-  createdDate!: Date;
-  snaps!: number;
-  imageUrl! : string;
+  @Input() faceSnap!: FaceSnap;
+  
   buttonText!: string;
 
   ngOnInit(){
-    this.title = 'Archibald';
-    this.description = 'Une super description';
-    this.createdDate = new Date();
-    this.snaps = 6;
-    this.imageUrl = 'https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg'
     this.buttonText = 'Oh snap!';
   }
 
   onSnap() {
     if (this.buttonText === 'Oh snap!'){
-      this.snaps++;
+      this.faceSnap.snaps++;
       this.buttonText = 'Oops, unSnap!';
     } else {
-      this.snaps--;
+      this.faceSnap.snaps--;
       this.buttonText = 'Oh snap!'
     }
   }
