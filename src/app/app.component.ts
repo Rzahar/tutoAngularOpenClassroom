63d4e1db3793e6f42440052ad2f4847aed88f92e
Rachid <rachidzahar@hotmail.fr>
2023-01-18 17:28:56 +0100

    add css evolution depending on snap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,31 +7,55 @@ import { FaceSnap } from './face-snap/models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-      this.mySnap = new FaceSnap(
-      'Archibald',
-      'Une super description',
-      'https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg',
-      new Date(),
-      0 );
-      this.myOtherSnap = new FaceSnap(
-        'Piano',
-        'Un super piano',
-        'https://i0.wp.com/apprendre-a-jouer-du-piano.com/wp-content/uploads/2016/09/piano-queue.jpg',
-        new Date(),
-        0
-      );
-      this.myLastSnap = new FaceSnap(
-        'Circuit training',
-        'Un super sport',
-        'https://medias.toutelanutrition.com/blog/2020/12/banner-circuit-training.jpg',
-        new Date(),
-        0
-      )
+    this.faceSnaps = [
+      {
+        title:'Archibald',
+        description:'Une super description',
+        imageUrl:'https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg',
+        createdDate: new Date(),
+        snaps: 150,
+      location: 'Lille'},
+      {
+        title:'Piano',
+        description:'Un super piano',
+        imageUrl:'https://i0.wp.com/apprendre-a-jouer-du-piano.com/wp-content/uploads/2016/09/piano-queue.jpg',
+        createdDate:new Date(),
+        snaps:255,
+        location:'pas à Lille'
+      },
+      {
+        title:'Circuit training',
+        description:'Un super sport',
+        imageUrl: 'https://medias.toutelanutrition.com/blog/2020/12/banner-circuit-training.jpg',
+        createdDate: new Date(),
+        snaps: 0
+      },
+      {
+        title:'Archibald',
+        description:'Une super description',
+        imageUrl:'https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+      location: 'Lille'},
+      {
+        title:'Piano',
+        description:'Un super piano',
+        imageUrl:'https://i0.wp.com/apprendre-a-jouer-du-piano.com/wp-content/uploads/2016/09/piano-queue.jpg',
+        createdDate:new Date(),
+        snaps:0,
+        location:'pas à Lille'
+      },
+      {
+        title:'Circuit training',
+        description:'Un super sport',
+        imageUrl: 'https://medias.toutelanutrition.com/blog/2020/12/banner-circuit-training.jpg',
+        createdDate: new Date(),
+        snaps: 0
+      }
+    ]
   }
 }
 
